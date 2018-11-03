217de053e3574295c16f1da7a5cbb7656e11fa03
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2018-11-03 20:36:40 +0700

    - 01-OOP

---
+++ students/nghia/01-OOP/OOP/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Person
+    {
+        protected string FirstName { get; set; }
+
+        protected string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public virtual void Hello()
+        {
+            Console.WriteLine($"Hello {FirstName} {LastName}");
+        }
+
+        public virtual void Hello(string key)
+        {
+            Console.WriteLine($"Hello {FirstName} {LastName}, {key}");
+        }
+    }
+}
---
+++ students/nghia/01-OOP/OOP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DongGoi();
+            // KeThua();
+            DaHinh();
+        }
+
+        static void DongGoi()
+        {
+            Person person = new Person("Super", "Dev");
+            person.Hello();
+        }
+
+        static void KeThua()
+        {
+            Teacher teacher1 = new Teacher();
+            Teacher teacher2 = new Teacher("Nghia", "Tran");
+            teacher1.Hello();
+            teacher2.Hello();
+        }
+
+        static void DaHinh()
+        {
+            List<Person> persons = new List<Person>();
+            Person person = new Person("Tuan", "Tran");
+            Teacher teacher = new Teacher("Nghia", "Tran");
+            persons.Add(person);
+            persons.Add(teacher);
+            foreach (var item in persons) item.Hello("NET");
+        }
+    }
+}
