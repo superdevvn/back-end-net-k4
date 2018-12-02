b4e906aa9b51abae7a3bad02f7379178aab2e789
tuantran664488@gmail.com <tuantran664488@gmail.com>
2018-12-02 15:39:43 +0700

    update learn CodeFirst

---
+++ students/tuan/04-CodeFirst/CodeFirst/CodeFirst/Program.cs
@@ -0,0 +1,57 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student= GetList();
+            Print(student);
+            Console.ReadKey();
+        }
+
+        public static void Init()
+        {
+            using(var context = new SuperDevDbContext())
+            {
+                var student = new Student()
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = "Minh",
+                    LastName = "Tuan",
+                };
+                context.Students.Add(student);
+                context.SaveChanges();
+            }
+        }
+
+        public static void CreateStudent(string firstName, string lastName)
+        {
+
+        }
+
+        public static List<Student> GetList()
+        {
+            using(var context = new SuperDevDbContext())
+            {
+                return context.Students.ToList();
+            }
+        }
+
+        public static void Print(List<Student> students)
+        {
+            foreach(var student in students)
+            {
+                Console.WriteLine($"Id: {student.Id}");
+                Console.WriteLine($"First Name: {student.FirstName}");
+                Console.WriteLine($"Last Name: {student.LastName}");
+            }
+        }
+    }
+}
---
+++ students/tuan/04-CodeFirst/CodeFirst/Models/Migrations/201812020756046_001.cs
@@ -0,0 +1,27 @@
+namespace Models.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _001 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Students");
+        }
+    }
+}
